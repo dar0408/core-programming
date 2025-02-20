579c7ef0bc42dd91b602e8f7ccc4346221ffc5e0
dar0408 <dsalgotra2021@gmail.com>
2025-02-20 11:06:53 +0530

    [Darpan] add : add comments

--- java-strings/level-two/RockPaper.java
+++ java-strings/level-two/RockPaper.java
@@ -1,51 +1,58 @@
 // Rock-Paper-Scissors is a game played between a minimum of two players. Each player can choose either rock, paper, or scissors. Here the game is played between a user and a computer. Based on the rules, either a player or a computer will win. Show the stats of player and computer win in a tabular format across multiple games. Also, show the winning percentage between the player and the computer.
-
 import java.util.Scanner;
 
 public class RockPaper {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of games: ");
-        int games = sc.nextInt();
-        String[][] stats = playGame(games);
-        displayStats(stats);
+        int games = sc.nextInt(); // Taking input for the number of games
+        String[][] stats = playGame(games); // Calling the playGame function to play the game
+        displayStats(stats); // Displaying the results
     }
 
     public static String[][] playGame(int games) {
-        String[] choices = {"Rock", "Paper", "Scissors"};
-        String[][] stats = new String[games][3];
+        String[] choices = {"Rock", "Paper", "Scissors"}; // Array to store choices
+        String[][] stats = new String[games][3]; // 2D array to store game stats
         int playerWins = 0;
         int computerWins = 0;
+        
         for (int i = 0; i < games; i++) {
-            int playerChoice = getPlayerChoice();
-            int computerChoice = getComputerChoice();
-            String winner = findWinner(choices[playerChoice], choices[computerChoice]);
+            int playerChoice = getPlayerChoice(); // Getting player choice
+            int computerChoice = getComputerChoice(); // Generating computer choice
+            String winner = findWinner(choices[playerChoice], choices[computerChoice]); // Determining winner
+            
             if (winner.equals("Player")) {
-                playerWins++;
+                playerWins++; // Increment player win count
             } else if (winner.equals("Computer")) {
-                computerWins++;
+                computerWins++; // Increment computer win count
             }
+            
+            // Storing choices and results in stats array
             stats[i][0] = choices[playerChoice];
             stats[i][1] = choices[computerChoice];
             stats[i][2] = winner;
         }
+        
+        // Storing final stats in the last row
         stats[games - 1][0] = "Player Wins: " + playerWins;
         stats[games - 1][1] = "Computer Wins: " + computerWins;
         stats[games - 1][2] = "Player Win Percentage: " + (playerWins * 100 / games) + "%";
+        
         return stats;
     }
 
     public static int getPlayerChoice() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your choice (0 for Rock, 1 for Paper, 2 for Scissors): ");
-        return sc.nextInt();
+        return sc.nextInt(); // Taking player's choice input
     }
 
     public static int getComputerChoice() {
-        return (int) (Math.random() * 3);
+        return (int) (Math.random() * 3); // Generating random choice for computer
     }
 
     public static String findWinner(String playerChoice, String computerChoice) {
+        // Determining winner based on game rules
         if (playerChoice.equals("Rock") && computerChoice.equals("Scissors")) {
             return "Player";
         } else if (playerChoice.equals("Scissors") && computerChoice.equals("Paper")) {
@@ -62,8 +69,10 @@ public class RockPaper {
     public static void displayStats(String[][] stats) {
         System.out.println("Player\tComputer\tWinner");
         for (int i = 0; i < stats.length - 1; i++) {
+            // Displaying each game's results
             System.out.println(stats[i][0] + "\t" + stats[i][1] + "\t" + stats[i][2]);
         }
+        // Displaying final statistics
         System.out.println(stats[stats.length - 1][0]);
         System.out.println(stats[stats.length - 1][1]);
         System.out.println(stats[stats.length - 1][2]);
--- java-strings/level-two/ShortAndLong.java
+++ java-strings/level-two/ShortAndLong.java
@@ -1,46 +1,48 @@
 // Write a program to split the text into words and find the shortest and longest strings in a given text
-
 import java.util.Scanner;
 
 public class ShortAndLong {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a string: ");
-        String str = sc.nextLine();
-        String[] words = splitWords(str);
-        String[][] wordsLength = splitWordsLength(words);
-        int[] shortLong = findShortLong(wordsLength);
+        System.out.println("Enter a string: "); // Prompt user for input
+        String str = sc.nextLine(); // Read the input string
+        String[] words = splitWords(str); // Split the string into words
+        String[][] wordsLength = splitWordsLength(words); // Store words with their lengths
+        int[] shortLong = findShortLong(wordsLength); // Find the shortest and longest words
+        
+        // Display shortest and longest words
         System.out.println("Shortest word: " + words[shortLong[0]]);
         System.out.println("Longest word: " + words[shortLong[1]]);
     }
 
     public static String[] splitWords(String str) {
-        return str.split(" ");
+        return str.split(" "); // Split the input string into words using space as a delimiter
     }
 
     public static String[][] splitWordsLength(String[] words) {
-        String[][] wordsLength = new String[words.length][2];
+        String[][] wordsLength = new String[words.length][2]; // Create a 2D array to store words and their lengths
         for (int i = 0; i < words.length; i++) {
-            wordsLength[i][0] = words[i];
-            wordsLength[i][1] = String.valueOf(words[i].length());
+            wordsLength[i][0] = words[i]; // Store word
+            wordsLength[i][1] = String.valueOf(words[i].length()); // Store word length
         }
         return wordsLength;
     }
 
     public static int[] findShortLong(String[][] wordsLength) {
-        int[] shortLong = new int[2];
-        shortLong[0] = 0;
-        shortLong[1] = 0;
+        int[] shortLong = new int[2]; // Array to store indices of shortest and longest words
+        shortLong[0] = 0; // Initialize shortest word index
+        shortLong[1] = 0; // Initialize longest word index
+        
         for (int i = 1; i < wordsLength.length; i++) {
-            int currentLength = Integer.parseInt(wordsLength[i][1]);
-            int shortLength = Integer.parseInt(wordsLength[shortLong[0]][1]);
-            int longLength = Integer.parseInt(wordsLength[shortLong[1]][1]);
+            int currentLength = Integer.parseInt(wordsLength[i][1]); // Current word length
+            int shortLength = Integer.parseInt(wordsLength[shortLong[0]][1]); // Shortest word length
+            int longLength = Integer.parseInt(wordsLength[shortLong[1]][1]); // Longest word length
             
             if (currentLength < shortLength) {
-                shortLong[0] = i;
+                shortLong[0] = i; // Update shortest word index
             }
             if (currentLength > longLength) {
-                shortLong[1] = i;
+                shortLong[1] = i; // Update longest word index
             }
         }
         return shortLong;
